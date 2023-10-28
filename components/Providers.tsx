85d8836ab5dfbb00a9ee156ed07fa9e3cd43537f
devgiljong <calf-love@naver.com>
2023-10-28 16:23:04 +0900

    :sparkles: Reactquery setup

---
+++ components/Providers.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+
+interface ProvidersProps {
+  children?: React.ReactNode;
+}
+
+const Providers = ({ children }: ProvidersProps) => {
+  const [client] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            suspense: true,
+          },
+        },
+      }),
+  );
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
