c88190978d736452d0a88b58e7d18b9791e0ab6a
devgiljong <calf-love@naver.com>
2023-11-01 01:51:21 +0900

    :sparkles: 프레이머모션과 스토리북이 별도 설정없이 호환되는 것 확인

--- components/XionButton.tsx
+++ components/XionButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { motion } from 'framer-motion';
 interface XionButtonProps {
   children?: React.ReactNode;
   variant?: 'default' | 'primary';
@@ -9,7 +9,24 @@ const XionButton = ({ children, variant = 'default' }: XionButtonProps) => {
   const defaultStyle = '';
   const primaryStyle = 'bg-purple-500 text-white rounded-md px-16 py-8';
   const buttonClass = variant === 'default' ? defaultStyle : primaryStyle;
-  return <button className={buttonClass}>{children}</button>;
+  return (
+    <div className="">
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 2,
+        }}
+      >
+        안녕하세요
+      </motion.div>
+      <button className={buttonClass}>{children}</button>
+    </div>
+  );
 };
 
 export default XionButton;
